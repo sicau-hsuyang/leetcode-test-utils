b0f05c1e2e87c35e91491d9caaa025c884dda145
yangxu <yangxu01@changba.com>
2023-09-03 16:15:38 +0800

    feat: 🎸 增加通用版本的并查集

--- test/dsu.spec.ts
+++ test/dsu.spec.ts
@@ -16,23 +16,23 @@ describe("GeneralDsu", () => {
   test("Union and Find", () => {
     dsu.init([1, 2, 3, 4, 5]);
     dsu.union(1, 2);
-    expect(dsu.find(1)).toBe(0); // 合并后应该返回 0
-    expect(dsu.find(2)).toBe(0); // 合并后应该返回 0
+    expect(dsu.find(1)).toBe(0); // 合并后应该返回 0，[[1,2], [3], [4], [5]]
+    expect(dsu.find(2)).toBe(0); // 合并后应该返回 0，[[1,2], [3], [4], [5]]
     expect(dsu.find(3)).toBe(2);
     dsu.union(4, 5);
-    expect(dsu.find(4)).toBe(3); // 合并后应该返回 3
-    expect(dsu.find(5)).toBe(3); // 合并后应该返回 3
+    expect(dsu.find(4)).toBe(3); // 合并后应该返回 3，[[1,2], [3], [4, 5]]
+    expect(dsu.find(5)).toBe(3); // 合并后应该返回 3，[[1,2], [3], [4, 5]]
     dsu.union(1, 4);
-    expect(dsu.find(1)).toBe(3); // 合并后应该返回 3
-    expect(dsu.find(4)).toBe(3); // 合并后应该返回 3
+    expect(dsu.find(1)).toBe(0); // 合并后应该返回 0, [[1,2, 4, 5], [3]]
+    expect(dsu.find(4)).toBe(0); // 合并后应该返回 0, [[1,2, 4, 5], [3]]
   });
 
   test("Counter", () => {
     dsu.init([1, 2, 3, 4, 5]);
-    dsu.union(1, 2);
-    dsu.union(4, 5);
-    dsu.union(1, 4);
-    expect(dsu.counter()).toBe(1); // 所有元素合并后应该只剩下一个根节点
+    dsu.union(1, 2); // [[1, 2], [3], [4], [5]]
+    dsu.union(4, 5); // [[1, 2], [3], [4, 5]]
+    dsu.union(1, 4); // [[1, 2, 4, 5], [3]]
+    expect(dsu.counter()).toBe(2); // 所有合并操作后应该只剩下2个根节点
   });
 
   test("Find non-existing element", () => {
@@ -42,11 +42,11 @@ describe("GeneralDsu", () => {
 
   test("Union already unioned sets", () => {
     dsu.init([1, 2, 3, 4, 5]);
-    dsu.union(1, 2);
-    dsu.union(4, 5);
-    dsu.union(1, 4);
+    dsu.union(1, 2); // [[1, 2], [3], [4], [5]]
+    dsu.union(4, 5); // [[1, 2], [3], [4, 5]]
+    dsu.union(1, 4); // [[1, 2, 4, 5], [3]]
     // 试图再次合并已经合并的集合，不应该影响结果
-    dsu.union(1, 4);
-    expect(dsu.counter()).toBe(1); // 所有元素合并后应该只剩下一个根节点
+    dsu.union(1, 4); // [[1, 2, 4, 5], [3]]
+    expect(dsu.counter()).toBe(2); // 所有元素合并后应该只剩下2个根节点
   });
 });
