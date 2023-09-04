03127b3d434063d1ba5eb2857ee1b73648d9c3fd
yangxu <yangxu01@changba.com>
2023-09-04 16:10:53 +0800

    feat: 🎸 增加merge的单元测试

--- test/merge.spec.ts
+++ test/merge.spec.ts
@@ -1,4 +1,4 @@
-import { merge } from "../src/merge";
+import { merge, mergeByAsc, mergeByDesc } from "../src/merge";
 
 describe("merge", () => {
   it("should merge two empty arrays into an empty array", () => {
@@ -34,5 +34,51 @@ describe("merge", () => {
     const arr2 = [2, 3, 5, 5];
     const result = merge(arr1, arr2, (val1, val2) => val1 < val2);
     expect(result).toEqual([1, 2, 2, 2, 3, 4, 5, 5]);
+  });
+});
+
+describe("merge asc", () => {
+  it("should merge an empty array with a non-empty array", () => {
+    const arr1: number[] = [];
+    const arr2 = [1, 3, 5];
+    const result = mergeByAsc(arr1, arr2);
+    expect(result).toEqual([1, 3, 5]);
+  });
+
+  it("should merge two non-empty arrays with a merge condition", () => {
+    const arr1 = [1, 3, 5];
+    const arr2 = [2, 4, 6];
+    const result = mergeByAsc(arr1, arr2);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it("should handle arrays with duplicate values", () => {
+    const arr1 = [1, 2, 2, 4];
+    const arr2 = [2, 3, 5, 5];
+    const result = mergeByAsc(arr1, arr2);
+    expect(result).toEqual([1, 2, 2, 2, 3, 4, 5, 5]);
+  });
+});
+
+describe("merge desc", () => {
+  it("should merge an empty array with a non-empty array", () => {
+    const arr1: number[] = [];
+    const arr2 = [5, 3, 1];
+    const result = mergeByDesc(arr1, arr2);
+    expect(result).toEqual([5, 3, 1]);
+  });
+
+  it("should merge two non-empty arrays with a merge condition", () => {
+    const arr1 = [5, 3, 1];
+    const arr2 = [6, 4, 2];
+    const result = mergeByDesc(arr1, arr2);
+    expect(result).toEqual([6, 5, 4, 3, 2, 1]);
+  });
+
+  it("should handle arrays with duplicate values", () => {
+    const arr1 = [4, 2, 2, 1];
+    const arr2 = [5, 5, 3, 2];
+    const result = mergeByDesc(arr1, arr2);
+    expect(result).toEqual([5, 5, 4, 3, 2, 2, 2, 1]);
   });
 });
